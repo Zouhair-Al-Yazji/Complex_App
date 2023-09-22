79449b6285a8da56dddc9d29dac711cc06e53dbc
Zouhair-Al-Yazji <alyazjyzhyr@gmail.com>
2023-09-22 17:54:44 +0300

    get post author

--- models/Post.js
+++ models/Post.js
@@ -1,5 +1,6 @@
-const postCollection = require('../config/db').db().collection('posts');
+const postsCollection = require('../config/db').db().collection('posts');
 const ObjectId = require('mongodb').ObjectId;
+const User = require('./User');
 
 let Post = function (data, userId) {
 	this.data = data;
@@ -41,7 +42,7 @@ Post.prototype.create = function () {
 		this.validate();
 		if (!this.errors.length) {
 			// save post into database
-			postCollection
+			postsCollection
 				.insertOne(this.data)
 				.then(() => {
 					resolve();
@@ -62,9 +63,56 @@ Post.findSingleById = function (id) {
 			reject();
 			return;
 		}
-		let post = await postCollection.findOne({ _id: new ObjectId(id) });
-		if (post) {
-			resolve(post);
+		// Aggregate is great when you need to perform complex or multiple operations. (db operations)
+		// Now aggregate is going to return data that makes sense from a MongoDB perspective, but maybe not just
+		// from a plain JavaScript perspective.
+		let posts = await postsCollection
+			.aggregate([
+				// So dollar sign Match will tell MongoDB that that's what we want to do.
+				// is basically describing documents that we want to match with.
+				// Okay, so that's our first operation. We're performing a match by the requested ID.
+				{ $match: { _id: new ObjectId(id) } },
+				/*
+				So currently we are within the posts collection, but what we want to do here is look up
+				documents from another collection, right?
+				Remember, our ultimate goal here is to pull in the relevant user account document so that we can
+				access its user name and email address for gravatars.
+				we're currently in the posts collection and now we're saying the document that we want to
+				look up should be pulled from the user's collection.
+				So this is saying when we're looking in the user's collection for matching documents, the local field or the field from within the current post item that we want to perform that match on is the author field,
+				So local means the current collection, that's the post collection, foreign means. The other collection that we're trying to look up within the field in those documents that we want to perform the lookup or match on is the ID field.
+
+				*/
+				{
+					$lookup: {
+						from: 'users',
+						localField: 'author',
+						foreignField: '_id',
+						as: 'authorDocument',
+					},
+				},
+				// So what project does is it allows us to spell out exactly what fields we want the resulting object
+				// to have.
+				{
+					$project: {
+						title: 1,
+						body: 1,
+						createdDate: 1,
+						author: { $arrayElemAt: ['$authorDocument', 0] },
+					},
+				},
+			])
+			.toArray();
+		// clean up author property in each post object
+		posts = posts.map((post) => {
+			post.author = {
+				username: post.author.username,
+				avatar: new User(post.author, true).avatar,
+			};
+			return post;
+		});
+		if (posts.length) {
+			resolve(posts[0]);
 		} else {
 			reject();
 		}
--- models/User.js
+++ models/User.js
@@ -1,22 +1,28 @@
-const bcrypt = require("bcryptjs");
-const usersCollection = require("../config/db").db().collection("users");
-const validator = require("validator");
-const md5 = require("md5");
+const bcrypt = require('bcryptjs');
+const usersCollection = require('../config/db').db().collection('users');
+const validator = require('validator');
+const md5 = require('md5');
 
-let User = function (data) {
+let User = function (data, getAvatar) {
 	this.data = data;
 	this.errors = [];
+	if (!getAvatar) {
+		getAvatar = false;
+	}
+	if (getAvatar) {
+		this.getAvatar();
+	}
 };
 
 User.prototype.cleanUp = function () {
-	if (typeof this.data.username != "string") {
-		this.data.username = "";
+	if (typeof this.data.username != 'string') {
+		this.data.username = '';
 	}
-	if (typeof this.data.email != "string") {
-		this.data.email = "";
+	if (typeof this.data.email != 'string') {
+		this.data.email = '';
 	}
-	if (typeof this.data.password != "string") {
-		this.data.password = "";
+	if (typeof this.data.password != 'string') {
+		this.data.password = '';
 	}
 
 	// get rid of any bogus properties
@@ -29,29 +35,29 @@ User.prototype.cleanUp = function () {
 
 User.prototype.validate = function () {
 	return new Promise(async (resolve, reject) => {
-		if (this.data.username === "") {
-			this.errors.push("You must provide a username");
+		if (this.data.username === '') {
+			this.errors.push('You must provide a username');
 		}
-		if (this.data.username.length != "" && !validator.isAlphanumeric(this.data.username)) {
-			this.errors.push("Username can only contain letters and numbers");
+		if (this.data.username.length != '' && !validator.isAlphanumeric(this.data.username)) {
+			this.errors.push('Username can only contain letters and numbers');
 		}
 		if (!validator.isEmail(this.data.email)) {
-			this.errors.push("You must provide a valid email");
+			this.errors.push('You must provide a valid email');
 		}
-		if (this.data.password === "") {
-			this.errors.push("You must provide a password");
+		if (this.data.password === '') {
+			this.errors.push('You must provide a password');
 		}
 		if (this.data.password.length > 0 && this.data.password.length < 12) {
-			this.errors.push("Password must be at least 12 characters.");
+			this.errors.push('Password must be at least 12 characters.');
 		}
 		if (this.data.password.length > 100) {
-			this.errors.push("Password cannot exceed 100 characters.");
+			this.errors.push('Password cannot exceed 100 characters.');
 		}
 		if (this.data.username.length > 0 && this.data.username.length < 3) {
-			this.errors.push("Username must be at least 3 characters.");
+			this.errors.push('Username must be at least 3 characters.');
 		}
 		if (this.data.username.length > 30) {
-			this.errors.push("Username cannot exceed 30 characters.");
+			this.errors.push('Username cannot exceed 30 characters.');
 		}
 		// Only if username is valid then check to see if it's already taken
 		if (
@@ -63,7 +69,7 @@ User.prototype.validate = function () {
 				username: this.data.username,
 			});
 			if (usernameExists) {
-				this.errors.push("That username is already taken.");
+				this.errors.push('That username is already taken.');
 			}
 		}
 
@@ -73,7 +79,7 @@ User.prototype.validate = function () {
 				email: this.data.email,
 			});
 			if (emailExists) {
-				this.errors.push("That email is already being used.");
+				this.errors.push('That email is already being used.');
 			}
 		}
 		resolve();
@@ -89,9 +95,9 @@ User.prototype.login = function () {
 
 		if (attemptedUser && bcrypt.compareSync(this.data.password, attemptedUser.password)) {
 			this.getAvatar();
-			resolve("congrats!!!");
+			resolve('congrats!!!');
 		} else {
-			reject("Invalid username / password.");
+			reject('Invalid username / password.');
 		}
 	});
 };
